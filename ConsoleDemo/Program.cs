eb5b31517746d6ae4eeb6cff4c4b625c468fae47
Jasper Kent <jasper_kent@hotmail.com>
2020-08-07 08:42:53 +0100

    Add project files.

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleDemo
+{
+    static class Extensions
+    {
+        public static T FromPrototype<T>(this object o, T prototype) => (T)o;
+    }
+
+    class Program
+    {
+        static object GetAnonymous()
+        {
+            return new { FirstName = "Mary", LastName = "Shelley" };
+        }
+
+        static void Main()
+        {
+            var anon = GetAnonymous().FromPrototype(new { FirstName = "", LastName = "" });
+          
+            Console.WriteLine(anon.FirstName);
+
+            var list = new object[0].Select(o => new { FirstName = "", LastName = "" }).ToList();
+
+            list.Add(anon);
+
+            foreach(var o in list)
+                Console.WriteLine(o);
+        }
+    }
+}
